25c352746818b8e67630c3b6fcc2d5a775a9aacb
lujiango <lujiang200829@126.com>
2017-03-22 22:05:03 +0800

    finsh connect zk, zookeeper's watcher, scan annocations

---
+++ src/main/java/com/github/xJetty/core/AnnotationScanner.java
@@ -0,0 +1,74 @@
+package com.github.xJetty.core;
+
+import org.apache.log4j.Logger;
+
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.FieldInfo;
+import javassist.bytecode.MethodInfo;
+import javassist.bytecode.annotation.Annotation;
+
+import com.github.xJetty.annotation.Entry;
+import com.github.xJetty.annotation.Register;
+import com.github.xJetty.annotation.ZKConf;
+import com.impetus.annovention.ClasspathDiscoverer;
+import com.impetus.annovention.Discoverer;
+import com.impetus.annovention.listener.ClassAnnotationObjectDiscoveryListener;
+import com.impetus.annovention.listener.FieldAnnotationObjectDiscoveryListener;
+import com.impetus.annovention.listener.MethodAnnotationObjectDiscoveryListener;
+
+public class AnnotationScanner {
+	private static final Logger LOG = Logger.getLogger(AnnotationScanner.class);
+
+	static void scanAnnotations() {
+		LOG.info("start scan annotations...");
+		long start = System.currentTimeMillis();
+		Discoverer discover = new ClasspathDiscoverer();
+		discover.addAnnotationListener(new MyClassAnnotationObjectDiscoveryListener());
+		discover.addAnnotationListener(new MyFieldAnnotationObjectDiscoveryListener());
+		discover.addAnnotationListener(new MyMethodAnnotationObjectDiscoveryListener());
+		discover.discover(true, true, true, true, true, true);
+		LOG.info("finish scan annotations in " + (System.currentTimeMillis() - start) + "ms");
+	}
+
+	static class MyClassAnnotationObjectDiscoveryListener implements
+			ClassAnnotationObjectDiscoveryListener {
+
+		public String[] supportedAnnotations() {
+			return new String[] { Entry.class.getName() };
+		}
+
+		public void discovered(ClassFile clazz, Annotation annotation) {
+
+		}
+
+	}
+
+	static class MyMethodAnnotationObjectDiscoveryListener implements
+			MethodAnnotationObjectDiscoveryListener {
+
+		public String[] supportedAnnotations() {
+			return new String[] { Register.class.getName() };
+		}
+
+		public void discovered(ClassFile clazz, MethodInfo method,
+				Annotation annotation) {
+
+		}
+
+	}
+
+	static class MyFieldAnnotationObjectDiscoveryListener implements
+			FieldAnnotationObjectDiscoveryListener {
+
+		public String[] supportedAnnotations() {
+			return new String[] { ZKConf.class.getName() };
+		}
+
+		public void discovered(ClassFile clazz, FieldInfo field,
+				Annotation annotation) {
+
+		}
+
+	}
+
+}
---
+++ src/main/java/com/github/xJetty/utils/SecurityUtils.java
@@ -0,0 +1,5 @@
+package com.github.xJetty.utils;
+
+public class SecurityUtils {
+	public static final String UTF_8 = "utf-8";
+}
