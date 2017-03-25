31358aa1676e870578a918809bd75b1ae5a6931e
lujiango <lujiang200829@126.com>
2017-03-25 21:47:24 +0800

    coding for routing

---
+++ src/main/java/org/github/xJetty/routing/Route.java
@@ -0,0 +1,41 @@
+package org.github.xJetty.routing;
+
+/**
+ * 
+ * @author lujiango
+ *
+ */
+public class Route {
+	private static Table routingTable = new Table();
+
+	public static Address routing(String serviceName) {
+		CycleQueue<Address> addresses = routingTable.get(serviceName);
+		return addresses.cycle();
+	}
+	
+	
+	public static void register(String serviceName, Address address) {
+		CycleQueue<Address> addresses = routingTable.get(serviceName);
+		if (null == addresses) {
+			addresses = new CycleQueue<Address>();
+			addresses.add(address);
+			routingTable.put(serviceName, addresses);
+		} else {
+			addresses.add(address);
+		}
+	}
+	
+	public static boolean unregister(String serviceName, Address address) {
+		CycleQueue<Address> addresses = routingTable.get(serviceName);
+		if (null == addresses) {
+			return false;
+		}
+		if (!addresses.remove(address)) {
+			return false;
+		}
+		if (addresses.isEmpty()) {
+			routingTable.remove(serviceName);
+		}
+		return true;
+	}
+}
