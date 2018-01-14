3e87ffba2d680a8247f4f93d506e4538334315b9
lujiango <lujiango>
2018-01-14 20:07:47 +0800

    启动HttpServer

--- src/main/java/org/github/x/jetty/http/HttpServer.java
+++ src/main/java/org/github/x/jetty/http/HttpServer.java
@@ -1,44 +1,63 @@
 package org.github.x.jetty.http;
 
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.log4j.Logger;
+import org.apache.zookeeper.CreateMode;
+import org.eclipse.jetty.io.ByteBufferPool;
+import org.eclipse.jetty.io.EndPoint;
+import org.eclipse.jetty.server.ConnectionFactory;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.util.component.LifeCycle.Listener;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.util.thread.Scheduler;
 import org.github.x.jetty.conf.Config;
+import org.github.x.jetty.conf.ZkClient;
 import org.github.x.jetty.core.Entry;
-import org.github.x.jetty.utils.Consts;
+import org.github.x.jetty.core.xJetty;
 
 /**
  * 
  * @author lujiango
  *
  */
-public class HttpServer extends Server {
+public class HttpServer  {
 	private static final Logger LOG = Logger.getLogger(HttpServer.class);
-	private static final String ADDRESS = "address";
+    private static Server server;
+    
+    private static ZkClient zkClient = Config.getSelf().getZkClient();
 
-	public HttpServer() {}
-
-	public HttpServer(String ip, int port) {
-		super(port);
-	}
-
+    
+    
 	@Entry(startup = -900)
-	public void startup() {
-		String tmpIp = Config.getAsString(ADDRESS, null);
-		if (tmpIp == null) {
-			LOG.fatal("Listen-ip is not set and xJetty will exit...");
+	public void doStart() throws URISyntaxException {
+		String addrInfo = Config.getAsString(Address.ADDRESS, null);
+		Address addr = new Address(addrInfo);
+		server = new Server();
+		if (addr.http()) {
+			ServerConnector connector = new ServerConnector(server);
+			connector.setHost(addr.getHost());
+			connector.setPort(addr.getPort());
+		} else if (addr.https()) {
+			SslContextFactory ctx = new SslContextFactory();
+			SslConnectionFactory connector = new SslConnectionFactory();
+		} else {
+			throw new URISyntaxException(addr.getAddress(), "Not supported");
 		}
-		if (!tmpIp.matches(Address.IP_REGEX)) {
-			LOG.fatal("Listen-ip pattern is unqualified and xJetty will exit...");
-		}
-
-		int tmpPort = Config.getAsInt(Consts.XJETTY_LISTEN_PORT, -1);
-
-		HttpServer server = new HttpServer(tmpIp, tmpPort);
+		
 		HandlerList hl = new HandlerList();
-
 		hl.setHandlers(new Handler[] { new SessionHandler(), new HttpHandler() });
 		server.setHandler(hl);
 		try {
@@ -46,6 +65,20 @@ public class HttpServer extends Server {
 			server.join();
 		} catch (Exception e) {
 			LOG.fatal("xJetty http server start exception: ", e);
+			System.exit(xJetty.XJETTY_EXIT_CODE);
 		}
+		
+		registerServer();
+		
+	}
+	
+	public void registerServer() {
+		LOG.info("Register server to routing-table.");
+		Pattern p = Pattern.compile("(/[^/]*){2}");
+        Matcher m = p.matcher(zkClient.getZkAddress().getPath());
+        m.find();
+        String engineRoomName = m.group();
+        zkClient.touch(engineRoomName.concat("/routing-table"), new byte[0], CreateMode.PERSISTENT);
+        LOG.info("Register server to routing-table successful.");
 	}
 }
